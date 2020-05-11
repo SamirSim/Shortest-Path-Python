7c5b2b4416857117ca53fe3e693c1052bebfeb84
Samir Si-Mohammed <samirsim98@gmail.com>
2020-05-11 16:34:52 +0200

    Updated algorithm

--- djikstra/djikstra.py
+++ djikstra/djikstra.py
@@ -2,8 +2,7 @@ import sys
 from random import seed
 import random 
 
-seed(3)
-
+MIGRATION_COST = 10
 
 def init_graph(dist, graphG, start):
 	for s in range(len(graphG)):
@@ -19,19 +18,20 @@ def find_min(dist, q):
 			vertex = s
 	return vertex
 
-def update_dist(dist, previous, edges, s1, s2, MIGRATION_COST, WEIGHT_MIGRATION, WEIGHT_OVERLOAD):
+def update_dist(dist, previous, edges, s1, s2, weight_migration, weight_overload):
 	"""
-	if dist[s2] > dist[s1] + WEIGHT_MIGRATION * edges[s1][s2] + edges[s2][s2]:
-		dist[s2] = dist[s1] + WEIGHT_MIGRATION * edges[s1][s2] + WEIGHT_OVERLOAD * edges[s2][s2]
+	if dist[s2] > dist[s1] + weight_migration * edges[s1][s2] + edges[s2][s2]:
+		dist[s2] = dist[s1] + weight_migration * edges[s1][s2] + weight_overload * edges[s2][s2]
 		previous[s2] = s1
 	"""
 	if dist[s2] > dist[s1] + edges[s1][s2] + edges[s2][s2]:
 		dist[s2] = dist[s1] + edges[s1][s2] + edges[s2][s2]
 		previous[s2] = s1
+	
 	return dist, previous
 
-def djikstra(vertices, edges, start, MIGRATION_COST, WEIGHT_MIGRATION, WEIGHT_OVERLOAD):
-	dist, previous, nbVertices, nodes, edges, probabilities = init(vertices, edges, MIGRATION_COST, WEIGHT_MIGRATION, WEIGHT_OVERLOAD)
+def djikstra(vertices, edges, start, weight_migration, weight_overload):
+	dist, previous, nbVertices, nodes, edges, probabilities = init(edges, weight_migration, weight_overload)
 	init_graph(dist, nodes, start)
 	q = nodes
 	while len(q) > 0:
@@ -39,10 +39,10 @@ def djikstra(vertices, edges, start, MIGRATION_COST, WEIGHT_MIGRATION, WEIGHT_OV
 		q.remove(s1)
 		for s2 in range(nbVertices):
 			if vertices[s1][s2] == 1:
-				dist, previous = update_dist(dist, previous, edges, s1, s2, MIGRATION_COST, WEIGHT_MIGRATION, WEIGHT_OVERLOAD)
+				dist, previous = update_dist(dist, previous, edges, s1, s2, weight_migration, weight_overload)
 	return previous, probabilities
 
-def shortest(edges, previous, start, target, MIGRATION_COST, WEIGHT_MIGRATION, WEIGHT_OVERLOAD, probabilities):
+def shortest(edges, previous, start, target, weight_migration, weight_overload, probabilities):
 	path = []
 	nbMigrations, nbOverloadedCells, distance = 0, 0, 0
 	s = target
@@ -50,7 +50,7 @@ def shortest(edges, previous, start, target, MIGRATION_COST, WEIGHT_MIGRATION, W
 		path.append(s)
 		#print(edges[s][s] / WEIGHT_OVERLOAD)
 		distance = distance + edges[s][previous[s]] + edges[s][s]
-		if edges[s][previous[s]] == WEIGHT_MIGRATION * MIGRATION_COST: 
+		if edges[s][previous[s]] == weight_migration * MIGRATION_COST: 
 			nbMigrations = nbMigrations + 1
 		s = previous[s]
 	path.append(start)
@@ -58,30 +58,31 @@ def shortest(edges, previous, start, target, MIGRATION_COST, WEIGHT_MIGRATION, W
 	for i in path:
 		#print(probabilities[i])
 		if probabilities[i] > 0.5:
-			
 			nbOverloadedCells = nbOverloadedCells + 1
 	return path, distance, nbMigrations, nbOverloadedCells
 
-def init(vertices, edges, MIGRATION_COST, WEIGHT_MIGRATION, WEIGHT_OVERLOAD):
+def init(edges, weight_migration, weight_overload):
 	dist = []
 	previous = []
 	nodes = []
-	for i in range(len(vertices)):
+	nbVertices = len(edges)
+	for i in range(nbVertices):
 		nodes.append(i)
 		dist.append(sys.maxsize)
 		previous.append(0)
 
-	nbVertices = len(vertices)
 	# seed random number generator
+	seed(1)
 	probabilities = []
+	
 	# generate random numbers between 0-1
 	for _ in range(nbVertices):
 		probabilities.append(random.uniform(0,1))
 	for i in range(nbVertices):
-		edges[i][i] = WEIGHT_OVERLOAD * probabilities[i]
+		edges[i][i] = weight_overload * probabilities[i]
 		#edges[i][i] = probabilities[i]
 		for j in range(nbVertices):
 			if edges[i][j] == MIGRATION_COST:
-				edges[i][j] = WEIGHT_MIGRATION * edges[i][j]
+				edges[i][j] = weight_migration * MIGRATION_COST
 
 	return dist, previous, nbVertices, nodes, edges, probabilities
